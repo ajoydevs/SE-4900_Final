90603cfc525d0016bec423b7d2fe8909a0f678c6
Arnold Joy <arnoldjoy007@gmail.com>
2026-05-03 19:23:27 -0500

    [CM#6] remove: client.ts

--- lib/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { requireSupabasePublicEnv } from "@/lib/supabase/env";
-
-export function createBrowserSupabase() {
-  const { url, publicKey } = requireSupabasePublicEnv();
-  return createBrowserClient(url, publicKey);
-}
